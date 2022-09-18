f6e971317e7dbe93ae228be1dc7628424d98dcc1
Aziz Utku Kagitci <azizutku@gmail.com>
2022-09-18 15:23:18 +0300

    Add ktlint

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,10 @@
+plugins {
+    id(BuildPlugins.KTLINT_PLUGIN).version(BuildPlugins.KTLINT_PLUGIN_VERSION)
+}
+
 allprojects {
     plugins.apply(BuildPlugins.DETEKT)
+    plugins.apply(BuildPlugins.KTLINT_PLUGIN)
 }
 
 tasks.register("clean", Delete::class) {
--- buildSrc/src/main/kotlin/BuildPlugins.kt
+++ buildSrc/src/main/kotlin/BuildPlugins.kt
@@ -3,7 +3,10 @@ object BuildPlugins {
     const val ANDROID_APPLICATION = "com.android.application"
     const val KOTLIN_ANDROID = "kotlin-android"
     const val KOTLIN_KAPT = "kotlin-kapt"
+    const val KTLINT_PLUGIN = "org.jlleitschuh.gradle.ktlint"
 
     const val DETEKT = "plugins.detekt"
+
+    const val KTLINT_PLUGIN_VERSION = "11.0.0"
 
 }
