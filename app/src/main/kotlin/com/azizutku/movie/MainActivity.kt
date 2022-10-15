88ab1bef96ee67a6c815ea0c1698a4962f1d0bad
Aziz Utku Kagitci <azizutku@gmail.com>
2022-10-15 17:14:29 +0300

    Add splash screen by using SplashScreen API

--- app/src/main/kotlin/com/azizutku/movie/MainActivity.kt
+++ app/src/main/kotlin/com/azizutku/movie/MainActivity.kt
@@ -1,11 +1,13 @@
 package com.azizutku.movie
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        installSplashScreen()
         setContentView(R.layout.activity_main)
     }
 }
