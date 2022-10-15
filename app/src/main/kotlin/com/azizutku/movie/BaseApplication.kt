2a25a04699bca003b5d586f5d6cdac3ad4a203f3
Aziz Utku Kagitci <azizutku@gmail.com>
2022-10-15 17:51:15 +0300

    Add base application for hilt and timber

---
+++ app/src/main/kotlin/com/azizutku/movie/BaseApplication.kt
@@ -0,0 +1,42 @@
+package com.azizutku.movie
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import timber.log.Timber
+import timber.log.Timber.DebugTree
+
+private const val FORMAT_TIMBER_CREATE_STACK_ELEMENT = "%s:%s"
+
+@HiltAndroidApp
+class BaseApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        CoroutineScope(Dispatchers.Default).launch {
+            initTimber()
+        }
+    }
+
+    private fun initTimber() {
+        if (BuildConfig.DEBUG) {
+            Timber.plant(
+                object : DebugTree() {
+                    override fun log(priority: Int, tag: String?, message: String, t: Throwable?) {
+                        super.log(priority, "Timber_$tag", message, t)
+                    }
+
+                    override fun createStackElementTag(element: StackTraceElement): String {
+                        return String.format(
+                            FORMAT_TIMBER_CREATE_STACK_ELEMENT,
+                            element.methodName,
+                            super.createStackElementTag(element)
+                        )
+                    }
+                }
+            )
+        }
+    }
+}
