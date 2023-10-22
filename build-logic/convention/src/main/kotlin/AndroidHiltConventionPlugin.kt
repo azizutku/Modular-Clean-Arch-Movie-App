3c32b80e7829361998fb738906e7de082445a78b
Aziz Utku Kagitci <azizutku@gmail.com>
2023-10-22 18:36:53 +0300

    Migrate to KSP from KAPT for Hilt

--- build-logic/convention/src/main/kotlin/AndroidHiltConventionPlugin.kt
+++ build-logic/convention/src/main/kotlin/AndroidHiltConventionPlugin.kt
@@ -2,32 +2,24 @@ import com.azizutku.movie.BuildPlugins
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.api.artifacts.VersionCatalogsExtension
-import org.gradle.api.plugins.ExtensionAware
 import org.gradle.kotlin.dsl.dependencies
 import org.gradle.kotlin.dsl.getByType
-import org.jetbrains.kotlin.gradle.plugin.KaptExtension
 
 class AndroidHiltConventionPlugin : Plugin<Project> {
     override fun apply(target: Project) {
         with(target) {
             with(pluginManager) {
                 apply(BuildPlugins.HILT_PLUGIN)
-                apply(BuildPlugins.KOTLIN_KAPT)
+                apply(BuildPlugins.KSP)
             }
 
             val libs = extensions.getByType<VersionCatalogsExtension>().named("libs")
             dependencies {
                 "implementation"(libs.findLibrary("hilt").get())
-                "kapt"(libs.findLibrary("hilt.compiler").get())
-                "kaptAndroidTest"(libs.findLibrary("hilt.compiler").get())
-                "kaptTest"(libs.findLibrary("hilt.compiler").get())
-            }
-            kapt {
-                correctErrorTypes = true
+                "ksp"(libs.findLibrary("hilt.compiler").get())
+                "kspAndroidTest"(libs.findLibrary("hilt.compiler").get())
+                "kspTest"(libs.findLibrary("hilt.compiler").get())
             }
         }
     }
 }
-
-fun Project.kapt(block: KaptExtension.() -> Unit): Unit =
-    (this as ExtensionAware).extensions.configure("kapt", block)
