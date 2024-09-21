acba2fe1db9d1ad73b258014bb4dd67a9cb6cdf7
Aziz Utku Kagitci <azizutku@gmail.com>
2024-09-21 19:38:19 +0300

    Set status bar color

--- core/ui/compose/src/main/kotlin/com/azizutku/movie/ui/theme/Theme.kt
+++ core/ui/compose/src/main/kotlin/com/azizutku/movie/ui/theme/Theme.kt
@@ -1,11 +1,16 @@
 package com.azizutku.movie.ui.theme
 
+import android.app.Activity
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.ReadOnlyComposable
+import androidx.compose.runtime.SideEffect
 import androidx.compose.runtime.staticCompositionLocalOf
+import androidx.compose.ui.graphics.toArgb
+import androidx.compose.ui.platform.LocalView
+import androidx.core.view.WindowCompat
 
 private val LocalColors = staticCompositionLocalOf { lightColorPalette }
 
@@ -15,6 +20,16 @@ fun AppTheme(
     content: @Composable () -> Unit
 ) {
     val colorPalette = if (darkTheme) darkColorPalette else lightColorPalette
+    val view = LocalView.current
+    if (!view.isInEditMode) {
+        SideEffect {
+            val window = (view.context as? Activity)?.window
+            window?.statusBarColor = colorPalette.background.toArgb()
+            window?.let {
+                WindowCompat.getInsetsController(it, view).isAppearanceLightStatusBars = darkTheme.not()
+            }
+        }
+    }
     CompositionLocalProvider(LocalColors provides colorPalette) {
         MaterialTheme(
             colors = colorPalette.material,
