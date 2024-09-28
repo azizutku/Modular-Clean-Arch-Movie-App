d31b16a3d839329b4140a9cd4bb6c3018c04f8bb
Aziz Utku Kagitci <azizutku@gmail.com>
2024-09-28 23:07:46 +0300

    Implement movie screen

--- feature/movie/presentation-compose/src/main/kotlin/com/azizutku/movie/feature/movie/MovieScreen.kt
+++ feature/movie/presentation-compose/src/main/kotlin/com/azizutku/movie/feature/movie/MovieScreen.kt
@@ -1,8 +1,18 @@
 package com.azizutku.movie.feature.movie
 
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.Divider
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Favorite
@@ -10,10 +20,20 @@ import androidx.compose.material.icons.filled.FavoriteBorder
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.PreviewParameter
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import coil.compose.AsyncImage
+import com.azizutku.feature.movie.common.domain.model.Movie
 import com.azizutku.feature.movie.common.presentation.MovieUiState
 import com.azizutku.feature.movie.common.presentation.MovieViewModel
 import com.azizutku.movie.core.common.extensions.orFalse
@@ -21,8 +41,16 @@ import com.azizutku.movie.ui.NavigationAction
 import com.azizutku.movie.ui.components.ErrorLoadingScaffold
 import com.azizutku.movie.ui.components.MovieAppTopAppBar
 import com.azizutku.movie.ui.components.TopAppBarAction
+import com.azizutku.movie.ui.theme.AppTheme
+import com.azizutku.movie.ui.theme.AppTypography
+import com.azizutku.movie.ui.theme.PreviewTheme
 import com.azizutku.feature.movie.common.R as movieCommonR
 import com.azizutku.movie.core.ui.common.R as uiCommonR
+
+private const val ImageWidthPercent = 0.60f
+private const val LogoWidthPercent = 0.50f
+private const val DividerWidthPercent = 0.70f
+private const val ImageAspectRatio = 1f / 1.5f
 
 @Composable
 internal fun MovieScreen(
@@ -39,7 +67,7 @@ internal fun MovieScreen(
     ErrorLoadingScaffold(
         viewModel = viewModel,
         errorPrimaryActionText = stringResource(uiCommonR.string.text_button_go_back),
-        onErrorPrimaryAction = { onNavigationAction(NavigationAction.OnBackButtonClicked) }
+        onErrorPrimaryAction = { onNavigationAction(NavigationAction.OnBackButtonClicked) },
     ) {
         MovieScreen(
             modifier = modifier,
@@ -62,6 +90,7 @@ private fun MovieScreen(
     if (uiState !is MovieUiState.Success) {
         return
     }
+    val movie = uiState.movie
     Column(modifier = modifier) {
         MovieAppTopAppBar(
             title = stringResource(movieCommonR.string.title_fragment_movie),
@@ -73,22 +102,110 @@ private fun MovieScreen(
                         Icons.Filled.FavoriteBorder
                     },
                     contentDescription = stringResource(
-                        movieCommonR.string.content_description_app_bar_action_favorites
+                        movieCommonR.string.content_description_app_bar_action_favorites,
                     ),
                 ) {
                     val shouldAdd = uiState.isMovieInWatchlist?.isInWatchlist.orFalse().not()
                     watchlistAction(shouldAdd)
-                }
+                },
             ),
             onNavigationIconClick = {
                 onNavigationAction(NavigationAction.OnBackButtonClicked)
-            }
+            },
         )
-        Box(
-            modifier = Modifier
-                .fillMaxSize()
-        ) {
-            Text("dsds")
+        if (movie != null) {
+            MovieContent(movie = movie)
         }
+    }
+}
+
+@Suppress("LongMethod")
+@Composable
+private fun MovieContent(
+    movie: Movie,
+    modifier: Modifier = Modifier,
+) {
+    Column(
+        modifier = modifier
+            .verticalScroll(rememberScrollState())
+            .fillMaxSize()
+            .padding(vertical = 16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+    ) {
+        AsyncImage(
+            model = movie.posterUrl,
+            contentDescription = stringResource(uiCommonR.string.content_description_alert_dialog_image),
+            placeholder = painterResource(id = uiCommonR.drawable.bg_placeholder_movie_image),
+            alignment = Alignment.Center,
+            contentScale = ContentScale.Crop,
+            modifier = Modifier
+                .fillMaxWidth(ImageWidthPercent)
+                .aspectRatio(ImageAspectRatio)
+                .clip(RoundedCornerShape(32.dp))
+                .shadow(4.dp),
+        )
+        Spacer(Modifier.height(20.dp))
+        Text(
+            text = movie.title,
+            style = AppTypography.h5,
+            color = MaterialTheme.colors.onSurface,
+            textAlign = TextAlign.Center,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 16.dp),
+        )
+        Text(
+            text = movie.subtitle,
+            style = AppTypography.subtitle2,
+            color = MaterialTheme.colors.onSurface,
+            textAlign = TextAlign.Center,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 16.dp),
+        )
+        Spacer(Modifier.height(12.dp))
+        Divider(modifier = Modifier.fillMaxWidth(fraction = DividerWidthPercent))
+        Spacer(Modifier.height(12.dp))
+        Text(
+            text = movie.tagline,
+            style = AppTypography.body1,
+            color = MaterialTheme.colors.onSurface,
+            textAlign = TextAlign.Center,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 16.dp),
+        )
+        Spacer(Modifier.height(12.dp))
+        Divider(modifier = Modifier.fillMaxWidth(fraction = DividerWidthPercent))
+        Spacer(Modifier.height(12.dp))
+        Text(
+            text = movie.description,
+            style = AppTypography.body2,
+            color = MaterialTheme.colors.onSurface,
+            textAlign = TextAlign.Center,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 16.dp),
+        )
+        Spacer(Modifier.height(16.dp))
+        Divider(modifier = Modifier.fillMaxWidth(fraction = DividerWidthPercent))
+        Spacer(Modifier.height(16.dp))
+        Image(
+            painter = painterResource(id = movieCommonR.drawable.ic_logo_tmdb),
+            contentDescription = stringResource(movieCommonR.string.content_description_tmdb_logo),
+            modifier = Modifier.fillMaxWidth(fraction = LogoWidthPercent),
+            contentScale = ContentScale.Inside,
+        )
+    }
+}
+
+@Composable
+@PreviewTheme
+private fun TrendingScreenPreview(
+    @PreviewParameter(MovieUiStatePreviewParameterProvider::class)
+    uiState: MovieUiState,
+) {
+    AppTheme {
+        MovieScreen(uiState = uiState)
     }
 }
---
+++ feature/movie/presentation-compose/src/main/kotlin/com/azizutku/movie/feature/movie/MovieUiStatePreviewParameterProvider.kt
@@ -0,0 +1,31 @@
+package com.azizutku.movie.feature.movie
+
+import androidx.compose.ui.tooling.preview.PreviewParameterProvider
+import com.azizutku.feature.movie.common.domain.model.Movie
+import com.azizutku.feature.movie.common.presentation.MovieUiState
+import com.azizutku.movie.core.model.watchlist.MovieWatchlistState
+
+class MovieUiStatePreviewParameterProvider : PreviewParameterProvider<MovieUiState> {
+    override val values: Sequence<MovieUiState> = sequenceOf(
+        MovieUiState.Success(
+            movie = Movie(
+                id = 1108566,
+                description = "An expat PI is hired to investigate a suspicious death in Crete, Greece, where " +
+                    "jealousies run deep amongst the victim's powerful family.",
+                posterUrl = "https://image.tmdb.org/t/p/w500/WiAEiqelck0NGWplhL5JQR12eg.jpg",
+                title = "Killer Heat",
+                tagline = "Jealousy can drive anyone to the edge.",
+                subtitle = "2024 | Crime | 6.2★"
+            ),
+            isMovieInWatchlist = MovieWatchlistState(
+                isInWatchlist = true,
+            )
+        ),
+        MovieUiState.Success(
+            movie = null,
+            isMovieInWatchlist = MovieWatchlistState(
+                isInWatchlist = false,
+            )
+        )
+    )
+}
