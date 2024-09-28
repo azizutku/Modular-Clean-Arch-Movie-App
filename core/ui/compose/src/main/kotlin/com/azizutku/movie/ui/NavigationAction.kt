eee853526d4a1c0f3548cce27a417362acec4782
Aziz Utku Kagitci <azizutku@gmail.com>
2024-09-28 22:04:38 +0300

    Fix handling navigation action

--- core/ui/compose/src/main/kotlin/com/azizutku/movie/ui/NavigationAction.kt
+++ core/ui/compose/src/main/kotlin/com/azizutku/movie/ui/NavigationAction.kt
@@ -1,5 +1,7 @@
 package com.azizutku.movie.ui
 
+import androidx.core.net.toUri
+import androidx.navigation.NavDeepLinkRequest
 import androidx.navigation.NavHostController
 
 sealed interface NavigationAction {
@@ -10,6 +12,11 @@ sealed interface NavigationAction {
 fun NavHostController.handleNavigationAction(navigationAction: NavigationAction) {
     when (navigationAction) {
         is NavigationAction.OnBackButtonClicked -> navigateUp()
-        is NavigationAction.NavigateTo -> navigate(navigationAction.deeplink)
+        is NavigationAction.NavigateTo -> {
+            val request = NavDeepLinkRequest.Builder
+                .fromUri(navigationAction.deeplink.toUri())
+                .build()
+            navigate(request)
+        }
     }
 }
